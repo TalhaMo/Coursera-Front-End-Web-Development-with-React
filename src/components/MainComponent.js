64e7c0053c1ec080d73c23214e2227aaeb4ede40
talhamo <talhamohamed23@yahoo.com>
2021-02-08 19:18:22 +0100

    Single Page Applications Part 1

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -4,15 +4,25 @@ import Menu from './MenuComponent';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
-import {DISHES} from '../shared/dishes'
+import Contact from './ContactComponent';
+import {DISHES} from '../shared/dishes';
+import { COMMENTS } from '../shared/comments';
+import { PROMOTIONS } from '../shared/promotions';
+import { LEADERS } from '../shared/leaders';
 
 function Main() {
 
   const [dishes, setDishes] = useState(DISHES);
+  const [comments, setComments] = useState(COMMENTS);
+  const [promotions, setPromotions] = useState(PROMOTIONS);
+  const [leaders, setLeaders] = useState(LEADERS)
+
   const HomePage = () => {
     return(
         <Home 
-        />
+              dish={dishes.filter((dish) => dish.featured)[0]}
+              promotion={promotions.filter((promo) => promo.featured)[0]}
+              leader={leaders.filter((leader) => leader.featured)[0]}/>
     );
   }
 
@@ -22,6 +32,7 @@ function Main() {
     <Switch>
         <Route path="/home" component={HomePage} />
         <Route exact path='/menu' component={()=><Menu dishes={dishes}/>} />
+        <Route exact path='/contactus' component={Contact}/>
         <Redirect to="/home" />
     </Switch>
     <Footer/>
