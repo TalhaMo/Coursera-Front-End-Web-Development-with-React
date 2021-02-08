4c319014b22ff1b63e3355facda69581395613d4
talhamo <talhamohamed23@yahoo.com>
2021-02-08 21:02:17 +0100

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -5,6 +5,7 @@ import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import Home from './HomeComponent';
 import Contact from './ContactComponent';
+import DishDetail from './DishdetailComponent';
 import {DISHES} from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -25,6 +26,12 @@ function Main() {
               leader={leaders.filter((leader) => leader.featured)[0]}/>
     );
   }
+  const DishWithId = ({match}) => {
+    return(
+        <DishDetail dish={dishes.filter((dish) => dish.id === parseInt(match.params.dishId,10))[0]} 
+          comments={comments.filter((comment) => comment.dishId === parseInt(match.params.dishId,10))} />
+    );
+  };
 
   return (
     <div>
@@ -33,6 +40,7 @@ function Main() {
         <Route path="/home" component={HomePage} />
         <Route exact path='/menu' component={()=><Menu dishes={dishes}/>} />
         <Route exact path='/contactus' component={Contact}/>
+        <Route path='/menu/:dishId' component={DishWithId} />
         <Redirect to="/home" />
     </Switch>
     <Footer/>
